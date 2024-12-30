db5783b8f8ea0388c314907e1adab1da226d3f65
RegisReggie <52007166+RegisReggie@users.noreply.github.com>
2024-12-29 20:34:42 -0600

    Update MenuUIManager.cs

--- Assets/MenuUIManager.cs
+++ Assets/MenuUIManager.cs
@@ -6,10 +6,53 @@ public class MenuUIManager : MonoBehaviour
 
     public SceneManage sceneManage;
 
+    public GameObject gameManager;
+    public GameObject levelManager;
+    public GameObject canvas;
+
+    public bool isGameManagerDestroyed;
+    public bool isLevelManagerDestroyed;
+    public bool isCanvasDestroyed;
+
 
     private void Awake()
     {
         sceneManage = GameObject.FindGameObjectWithTag("SceneManager").GetComponent<SceneManage>();
+        isGameManagerDestroyed = false;
+        isLevelManagerDestroyed = false;
+        isCanvasDestroyed = false;
+    }
+
+
+    public void Update()
+    {
+        if(gameManager == null && !isGameManagerDestroyed)
+        {
+            gameManager = GameObject.FindGameObjectWithTag("GameManager");
+        }
+        else if(gameManager != null && !isGameManagerDestroyed)
+        {
+            Destroy(gameManager);
+            isGameManagerDestroyed = true;
+        }
+
+        if(levelManager == null && !isLevelManagerDestroyed)
+        {
+            levelManager = GameObject.FindGameObjectWithTag("LevelManager");
+        }else if(levelManager != null && !isLevelManagerDestroyed)
+        {
+            Destroy(levelManager);
+            isLevelManagerDestroyed = true;
+        }
+
+        if (canvas == null && !isCanvasDestroyed)
+        {
+            canvas = GameObject.FindGameObjectWithTag("Canvas");
+        }else if (canvas != null && !isCanvasDestroyed)
+        {
+            Destroy(canvas);
+            isCanvasDestroyed = true;
+        }
     }
 
     public void StartGameButton()
