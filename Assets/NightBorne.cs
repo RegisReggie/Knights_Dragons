126627fdbb51c1c6ce5dc2351a952d22cdc9db80
RegisReggie <52007166+RegisReggie@users.noreply.github.com>
2025-01-02 23:35:20 -0600

    Added Exploding Damage To NightBorne

--- Assets/NightBorne.cs
+++ Assets/NightBorne.cs
@@ -7,6 +7,9 @@ public class NightBorne : MonoBehaviour
     public Animator animator;
     public bool isExploding;
 
+    public GameObject attackPosition;
+    public float attackRange;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -32,11 +35,42 @@ public class NightBorne : MonoBehaviour
         }
     }
 
-    IEnumerator Explode()
+    public void ExplodeAttack()
+    {
+
+
+        Collider2D[] hitPlayer = Physics2D.OverlapCircleAll(attackPosition.transform.position, attackRange);
+
+        foreach (Collider2D player in hitPlayer)
+        {
+            if (player.CompareTag("Player"))
+            {
+                // Call a method to damage the enemy.
+                player.GetComponent<PlayerController>().TakeDamage(40f);
+                Debug.Log("Hit");
+            }
+
+        }
+    }
+
+
+        IEnumerator Explode()
     {
         isExploding = false;
         animator.SetTrigger("Death");
         yield return new WaitForSeconds(1.33f);
         Destroy(this.gameObject);
     }
+
+
+    private void OnDrawGizmosSelected()
+
+    {
+
+        Gizmos.color = Color.blue; // Set gizmo color
+
+        Gizmos.DrawWireSphere(attackPosition.transform.position, attackRange); // Draw circle at attack origin
+
+    }
+
 }
